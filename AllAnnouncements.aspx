677b999eba85509db11a32847fdcaefa28323faf
nzlkus <nzlks95@gmail.com>
2018-05-13 22:59:16 +0300

    Website Version 1.1

--- AllAnnouncements.aspx
+++ AllAnnouncements.aspx
@@ -1,6 +1,7 @@
-﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageAnnouncement.master" AutoEventWireup="true" CodeFile="AllAnnouncements.aspx.cs" Inherits="Announcements" %>
+﻿<%@ Page Title="" Language="C#" MasterPageFile="~/MasterPageAnnouncement.master" AutoEventWireup="true" CodeFile="AllAnnouncements.aspx.cs" Inherits="AllAnnouncements" %>
 
 <asp:Content ID="Content1" ContentPlaceHolderID="head" Runat="Server">
+
 </asp:Content>
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
     <div runat="server" style=" margin-left:200px; margin-top:auto; "  >
@@ -11,17 +12,20 @@
                    r="black" onmouseout="this.start()"  width="100%" height="300" direction="Up"
                    scrollamount="3" scrolldelay="60" loop="99999" font="white" >
 
-
+            <asp:Label ID="lblmes" runat="server" Text="Label" Visible="False" ForeColor="Red" Font-Bold="True"></asp:Label>
             <asp:DataList ID="dtAnnouncemnets" runat="server">
 
                  <ItemTemplate>                       
                             <table  >
                                 <tr>                                   
                                     <td align="right" style="color:#ffbe0a">
-                                        <%# FindDate(Eval("PublishDate")) %>&nbsp-
-                                    </td>&nbsp
+                                        <%# FindDate(Eval("PublishDate")) %>&nbsp- 
+                                    </td>
                                     <td>
                                         <%#Eval("AnnContent") %>
+                                    </td>
+                                    <td style="color:red; font-weight: bold; font-size:medium"  >
+                                        <%# Alert(Eval("ExpDate")) %>
                                     </td>
                                 </tr>
                             </table>
--- AllAnnouncements.aspx.cs
+++ AllAnnouncements.aspx.cs
@@ -4,36 +4,77 @@ using System.Linq;
 using System.Web;
 using System.Web.UI;
 using System.Web.UI.WebControls;
-
-public partial class Announcements : System.Web.UI.Page
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+public partial class AllAnnouncements : System.Web.UI.Page
 {
+    MySql.Data.MySqlClient.MySqlConnection conn;
+    MySql.Data.MySqlClient.MySqlCommand cmd;
+    MySql.Data.MySqlClient.MySqlDataReader rd;
+    String queryStr;
+
+    MySql.Data.MySqlClient.MySqlConnection connAnn;
+    MySql.Data.MySqlClient.MySqlCommand cmdAnn;
+    MySql.Data.MySqlClient.MySqlDataReader rdAnn;
+    String queryStrAnn;
+
+    MySql.Data.MySqlClient.MySqlConnection connUp;
+    MySql.Data.MySqlClient.MySqlCommand cmdUp;
+    MySql.Data.MySqlClient.MySqlDataReader rdUp;
+    String queryStrUp;
+    int id;
     protected void Page_Load(object sender, EventArgs e)
     {
-        AdroitOnlineTimeTableEntities db = new AdroitOnlineTimeTableEntities();
-        Teachers teacher = (Teachers)Session["Teachers"];
 
-        Teachers t = (from x in db.Teachers
-                      where x.TeacherID == teacher.TeacherID
-                      select x).SingleOrDefault();
-        if (!IsPostBack)
+        string constr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        conn = new MySqlConnection(constr);
+        conn.Open();
+        cmd = new MySqlCommand("SELECT *  FROM Teachers where Email= '" + Session["Email"].ToString() + "' ", conn);
+        rd = cmd.ExecuteReader();
+
+        if (rd.Read())
         {
-            var query = (from x in db.Teacher_Ann
-                         where x.TeacherID == teacher.TeacherID && x.IsActive == true
 
-                         select new
-                         {
-                           x.AnnouncementID,
-                           x.AnnContent,
-                           x.PublishDate,
-                           x.ExpDate
-
-                         }).ToList();
-
-            var SortedList = query.OrderBy(o => o.PublishDate).ToList();
-
-            dtAnnouncemnets.DataSource = SortedList;
-            dtAnnouncemnets.DataBind();
+            id = Convert.ToInt32(rd.GetString("TeacherID"));
+            conn.Close();
+            rd.Close();
         }
+        conn.Close();
+        int x = 0;
+        string tnow = Convert.ToString(DateTime.Now);
+        string createddate = Convert.ToDateTime(tnow).ToString("yyyy-MM-dd HH:mm:ss");
+        string constrUp = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        connUp = new MySqlConnection(constrUp);
+        connUp.Open();
+        cmdUp = new MySqlCommand("UPDATE Teacher_Ann SET IsActive=@a1 WHERE Expdate<@a2 and TeacherID=@a3",connUp);
+        cmdUp.Parameters.Add("a1",x);
+        cmdUp.Parameters.Add("a2", createddate);
+        cmdUp.Parameters.Add("a3", id);
+        cmdUp.ExecuteNonQuery();
+        connUp.Close();
+
+        
+
+
+        string constrAnn = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        connAnn = new MySqlConnection(constrAnn);
+        connAnn.Open();
+        cmdAnn = new MySqlCommand("SELECT * FROM Teacher_Ann where IsActive=1 and TeacherID='"+id+"' and ExpDate>= '"+ createddate + "' order by PublishDate ", connAnn);
+        MySqlDataAdapter sda = new MySqlDataAdapter(cmdAnn);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        if (dt.Rows.Count==0)
+        {
+            lblmes.Visible = true;
+            lblmes.Text = " There is no announcement!";
+        }
+        dtAnnouncemnets.DataSource = dt;
+        dtAnnouncemnets.DataBind();
+        rdAnn = cmdAnn.ExecuteReader();
+        connAnn.Close();
+
+
     }
 
     public string FindDate(object date)
@@ -41,5 +82,25 @@ public partial class Announcements : System.Web.UI.Page
         DateTime dt = Convert.ToDateTime(date);
         string res = Convert.ToString(dt.Day) + "." + Convert.ToString(dt.Month) + "." + Convert.ToString(dt.Year);
         return res;
+    }
+
+    public string Alert(object date)
+    {
+        DateTime dt = Convert.ToDateTime(date);
+        DateTime n = DateTime.Now;
+        if (dt.Year == n.Year)
+        {
+            if (dt.Month == n.Month)
+            {
+                if (dt.Day - n.Day <= 1)
+                {
+                    string res = "" + "" + "Last Day!";
+
+                    return res;
+                }
+            }
+        }
+        return " ";
+
     }
 }
