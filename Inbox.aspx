09eba024e93347d430228d4ff9896489c884dd11
OzlemKilicc <33120956+OzlemKilicc@users.noreply.github.com>
2018-05-13 23:04:17 +0300

    Website Version 1.1

--- Inbox.aspx
+++ Inbox.aspx
@@ -5,19 +5,20 @@
 <asp:Content ID="Content2" ContentPlaceHolderID="ContentPlaceHolder1" Runat="Server">
 
     <div runat="server" style=" margin-left:200px; margin-top:auto; "  >
-        <asp:GridView ID="grdEmail" runat="server" AutoGenerateColumns="False" BackColor="#CCCCCC" BorderColor="#262626" BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2" ForeColor="#262626" Height="100%" width="100%"  Style="overflow-y: scroll; overflow:auto; " OnRowCancelingEdit="grdEmail_RowCancelingEdit" OnRowEditing="grdEmail_RowEditing" OnRowUpdating="grdEmail_RowUpdating">
+        <center>  <asp:Label ID="lblmes" runat="server" Text="Label" Visible="False" Font-Bold="True" Font-Italic="True" ForeColor="#242424" Font-Size="Large"></asp:Label></center>
+        <asp:GridView ID="grdEmail" runat="server" AutoGenerateColumns="False" BackColor="#CCCCCC" BorderColor="#262626" BorderStyle="Solid" BorderWidth="3px" CellPadding="4" CellSpacing="2" ForeColor="#262626" Height="100%" width="100%"  Style="overflow-y: scroll; overflow:auto; " OnRowCancelingEdit="grdEmail_RowCancelingEdit" OnRowEditing="grdEmail_RowEditing" OnRowUpdating="grdEmail_RowUpdating"> <%--OnRowDataBound="OnRowDataBound--%>
             <Columns>
                 <asp:TemplateField HeaderText="Id" Visible="False">
                     <ItemTemplate>
                         <asp:Label ID="lblId" runat="server" Text='<%# Bind("MessageID") %>'></asp:Label>
                     </ItemTemplate>
                 </asp:TemplateField>
-                <asp:TemplateField HeaderText="Content">
-                    <ItemTemplate>
-                        <asp:Label ID="lblKonu" runat="server" Text='<%# Bind("MessageContent") %>'></asp:Label>
-                    </ItemTemplate>
-                    <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
-                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                <asp:TemplateField HeaderText="Image">
+                     <ItemTemplate>
+                         <asp:Image ID="Messageimg" runat="server" imageURL='<%# Bind("PhotoUrl") %>' Height="90px" Width="90px" />
+                     </ItemTemplate>
+                     <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                     <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
                 </asp:TemplateField>
                 <asp:TemplateField HeaderText="Sender">
                     <ItemTemplate>
@@ -26,6 +27,13 @@
                     <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
                     <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
                 </asp:TemplateField>
+                <asp:TemplateField HeaderText="Content">
+                    <ItemTemplate>
+                        <asp:Label ID="lblKonu" runat="server" Text='<%# Bind("MessageContent") %>'></asp:Label>
+                    </ItemTemplate>
+                    <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                    <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
+                </asp:TemplateField>               
               
                 <asp:TemplateField HeaderText="Date">
                     <ItemTemplate>
@@ -45,6 +53,7 @@
                     <HeaderStyle HorizontalAlign="Center" VerticalAlign="Middle" />
                     <ItemStyle HorizontalAlign="Center" VerticalAlign="Middle" />
                 </asp:TemplateField>
+                 
             </Columns>
             <FooterStyle BackColor="#CCCCCC" />
             <HeaderStyle BackColor="#262626" Font-Bold="True" ForeColor="White" />
@@ -57,11 +66,9 @@
             <SortedDescendingHeaderStyle BackColor="#383838" />
         </asp:GridView>
 
-   
-
     </div>
        <div style="margin-left:1295px">
-            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" BackColor="#262626" Font-Bold="True" ForeColor="White"  />
+            <asp:Button ID="btnDelete" runat="server" Text="Delete" OnClick="btnDelete_Click" BackColor="#262626" Font-Bold="True" ForeColor="White" Visible="False"  />
         </div>
        <div runat="server" style=" margin-left:200px; margin-top:auto; " >
            
@@ -72,7 +79,7 @@
         <asp:TextBox ID="txtMail" runat="server" Height="53px" TextMode="MultiLine" Width="404px" Visible="False"></asp:TextBox>
         <br /><br />
         <asp:Button ID="btnSendEmail" runat="server" OnClick="btnSendEmail_Click" Text="Send" Visible="False" BackColor="#262626" Font-Bold="True" Font-Size="Medium" ForeColor="White" />
-           <asp:Label ID="lblmessage" runat="server" Font-Bold="True" ForeColor="#262626" Text="Label"></asp:Label>
+           <asp:Label ID="lblmessage" runat="server" Font-Bold="True" ForeColor="#262626" Text="Label" Visible="false"></asp:Label>
     </div>
 </asp:Content>
 
--- Inbox.aspx.cs
+++ Inbox.aspx.cs
@@ -6,171 +6,255 @@ using System.Web.UI;
 using System.Net.Mail;
 using System.Net;
 using System.Web.UI.WebControls;
-
+using System.Configuration;
+using MySql.Data.MySqlClient;
+using System.Data;
+using System.IO;
 
 public partial class Inbox : System.Web.UI.Page
-{public static string receiver;
+{
+    int messageid;
+    public static string receiver;
+    MySql.Data.MySqlClient.MySqlConnection conn;
+    MySql.Data.MySqlClient.MySqlCommand cmd;
+    MySql.Data.MySqlClient.MySqlDataReader rd;
+    String queryStr;
+    MySql.Data.MySqlClient.MySqlConnection imageconn;
+    MySql.Data.MySqlClient.MySqlCommand imagecmd;
+    MySql.Data.MySqlClient.MySqlDataReader imagerd;
+
+    MySql.Data.MySqlClient.MySqlConnection urlconn;
+    MySql.Data.MySqlClient.MySqlCommand urlcmd;
+    MySql.Data.MySqlClient.MySqlDataReader urlrd;
+   
+    String imagequeryStr;
+    String Dt;
+    int id; //teacher id
+    int t = 1; //true value
+    string TID; //teacherId
+    string TEmail; //teacherEmail
+
     protected void Page_Load(object sender, EventArgs e)
     {
-        AdroitOnlineTimeTableEntities db = new AdroitOnlineTimeTableEntities();
-        Teachers teacher = (Teachers)Session["Teachers"];
-        lblmessage.Visible = false;
-        //ServiceReferenceDersler.Service1Soap DERSLER = new ServiceReferenceDersler.Service1SoapClient();
+        btnDelete.Visible = false;
+        string constr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        conn = new MySqlConnection(constr);
+        conn.Open();
+        cmd = new MySqlCommand("SELECT *  FROM Teachers where Email= '" + Session["Email"].ToString() + "' ", conn);
+        rd = cmd.ExecuteReader();
 
-        //DERSLER.XML_3_OBS.
-        Teachers t = (from x in db.Teachers
-                      where x.TeacherID == teacher.TeacherID
-                      select x).SingleOrDefault();
+        if (rd.Read())
+        {
+
+            id = Convert.ToInt32(rd.GetString("TeacherID"));
+            rd.Close();
+        }
+        conn.Close();
+
         if (!IsPostBack)
         {
-            var query = (from x in db.Messages
-                         where x.TeacherID == teacher.TeacherID && x.IsActive == true && x.SenderName != t.Email && x.Draft != true
+            int idd = 1;
+            string imageconstr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+            imageconn = new MySqlConnection(imageconstr);
+            imageconn.Open();
+            imagecmd = new MySqlCommand("SELECT * FROM Messages WHERE IsActive= '" + idd + "' and TeacherID='"+ TID +"' ", imageconn);
+            MySqlDataAdapter imagesda = new MySqlDataAdapter(imagecmd);
+            imagerd = imagecmd.ExecuteReader();
 
-                         select new
-                         {
-                             x.SenderName,
-                             x.MessageContent,
-                             x.TimeSent,
-                             x.MessageID
+            while (imagerd.Read())
+            {
+                int row = Convert.ToInt32(imagerd["MessageID"].ToString());
+                if (imagerd["Image"] != DBNull.Value)
+                {
+                    byte[] imgg = (byte[])(imagerd["Image"]);
+                    if (imgg != null)
+                    {
 
+                        MemoryStream mstream = new MemoryStream(imgg);
+                        string url = "data:image/jpeg;base64," + Convert.ToBase64String(imgg);
 
-                         }).ToList();
+                        string urlconstr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+                        urlconn = new MySqlConnection(urlconstr);
+                        urlconn.Open();
+                        urlcmd = new MySqlCommand("UPDATE Messages SET PhotoUrl=@a1 WHERE MessageID=@a2", urlconn);
+                        urlcmd.Parameters.Add("a1", url);
+                        urlcmd.Parameters.Add("a2", row);
 
-            var SortedList = query.OrderBy(o => o.TimeSent).ToList();
+                        urlcmd.ExecuteNonQuery();
+                        urlconn.Close();
+                        urlcmd.Dispose();
 
-            grdEmail.DataSource = SortedList;
+                    }
+                }
+            }
+            imageconn.Close();
+
+            lblmessage.Visible = false;
+            conn.Open();
+            cmd = new MySqlCommand("SELECT SenderName,MessageContent,TimeSent,MessageID,PhotoUrl FROM Messages where IsActive= '" + t + "' and TeacherID= '" + id + "' and SenderName!= '" + Session["Email"] + "' ORDER BY TimeSent DESC", conn);
+            MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+            DataTable dt = new DataTable();
+            sda.Fill(dt);
+            if (dt.Rows.Count!=0)
+            {
+                btnDelete.Visible = true;
+            }
+            else
+            {
+                lblmes.Visible = true;
+                lblmes.Text = "There is no Email!";
+            }
+            grdEmail.DataSource = dt;
             grdEmail.DataBind();
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            cmd.Dispose();
+
         }
     }
 
     protected void btnDelete_Click(object sender, EventArgs e)
     {
-        
-        AdroitOnlineTimeTableEntities db = new AdroitOnlineTimeTableEntities();
-        Teachers teacher = (Teachers)Session["Teachers"];
-
+     
         for (int i = 0; i < grdEmail.Rows.Count; i++)
         {
             GridViewRow row = (GridViewRow)grdEmail.Rows[i];
             Label lblid = (Label)row.FindControl("lblId");
             int id1 = Convert.ToInt32(lblid.Text);
             CheckBox cbRequest = (CheckBox)row.FindControl("cbDelete");
-           
+
             if (cbRequest != null && cbRequest.Checked)
             {
-                Messages m1 = (from x in db.Messages
-                               where x.MessageID == id1 && x.IsActive == true
-                               select x).SingleOrDefault();
-                if (m1 != null)
-                {
-                    m1.IsActive = false;
-                    db.SaveChanges();
-                }
+                int fals = 0;
+                int tru = 1;
+                string constr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+                conn = new MySqlConnection(constr);
+                conn.Open();
+                cmd = new MySqlCommand("UPDATE Messages SET IsActive=@a1 WHERE MessageID=@a2 and IsActive=@a3", conn);
+                cmd.Parameters.Add("a1", fals);
+                cmd.Parameters.Add("a2", id1);
+                cmd.Parameters.Add("a3", tru);
+                cmd.ExecuteNonQuery();
+                conn.Close();
+                cmd.Dispose();
+               
 
             }
 
-           
-        } Response.Redirect(Request.RawUrl);
+
+        }
+        Response.Redirect(Request.RawUrl);
     }
 
     protected void grdEmail_RowCancelingEdit(object sender, GridViewCancelEditEventArgs e)
     {
         grdEmail.EditIndex = -1;
-        AdroitOnlineTimeTableEntities db = new AdroitOnlineTimeTableEntities();
-        Teachers teacher = (Teachers)Session["Teachers"];
-
-        Teachers t = (from x in db.Teachers
-                      where x.TeacherID == teacher.TeacherID
-                      select x).SingleOrDefault();
-        var query = (from x in db.Messages
-                     where x.TeacherID == teacher.TeacherID && x.IsActive == true && x.SenderName != t.Email && x.Draft != true
-
-                     select new
-                     {
-                         x.SenderName,
-                         x.MessageContent,
-                         x.TimeSent,
-                         x.MessageID
-
-
-                     }).ToList();
-
-
-        grdEmail.DataSource = query;
+        string constr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        conn = new MySqlConnection(constr);
+        conn.Open();
+        cmd = new MySqlCommand("SELECT SenderName,MessageContent,TimeSent,MessageID,PhotoUrl FROM Messages where IsActive= '" + t + "' and TeacherID= '" + id + "' and SenderName!= '" + Session["Email"] + "' ORDER BY TimeSent DESC ", conn);
+        MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        grdEmail.DataSource = dt;
         grdEmail.DataBind();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        cmd.Dispose();
         txtMail.Visible = false;
         btnSendEmail.Visible = false;
         txtReceiver.Visible = false;
         txtSubject.Visible = false;
         lblsubject.Visible = false;
         lblmsg.Visible = false;
+        lblmessage.Visible = false;
     }
 
     protected void grdEmail_RowEditing(object sender, GridViewEditEventArgs e)
     {
         grdEmail.EditIndex = e.NewEditIndex;
-        AdroitOnlineTimeTableEntities db = new AdroitOnlineTimeTableEntities();
-        Teachers teacher = (Teachers)Session["Teachers"];
-
-        Teachers t = (from x in db.Teachers
-                      where x.TeacherID == teacher.TeacherID
-                      select x).SingleOrDefault();
-        var query = (from x in db.Messages
-                     where x.TeacherID == teacher.TeacherID && x.IsActive == true && x.SenderName != t.Email && x.Draft != true
-
-                     select new
-                     {
-                         x.SenderName,
-                         x.MessageContent,
-                         x.TimeSent,
-                         x.MessageID
-
-
-                     }).ToList();
-
-
-        grdEmail.DataSource = query;
+        string constr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        conn = new MySqlConnection(constr);
+        conn.Open();
+        cmd = new MySqlCommand("SELECT SenderName,MessageContent,TimeSent,MessageID,PhotoUrl FROM Messages where IsActive= '" + t + "' and TeacherID= '" + id + "' and SenderName!= '" + Session["Email"] + "' ORDER BY TimeSent DESC", conn);
+        MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        grdEmail.DataSource = dt;
         grdEmail.DataBind();
+        cmd.ExecuteNonQuery();
+        conn.Close();
+        cmd.Dispose();
 
     }
 
     protected void grdEmail_RowUpdating(object sender, GridViewUpdateEventArgs e)
     {
 
-        AdroitOnlineTimeTableEntities db = new AdroitOnlineTimeTableEntities();
         GridViewRow row = (GridViewRow)grdEmail.Rows[e.RowIndex];
         Label lblid = (Label)row.FindControl("lblId");
         int id1 = Convert.ToInt32(lblid.Text);
-        Messages m1 = (from x in db.Messages
-                       where x.MessageID == id1 && x.IsActive == true
-                       select x).SingleOrDefault();
 
+        string constr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        conn = new MySqlConnection(constr);
+        conn.Open();
+        cmd = new MySqlCommand("SELECT SenderName,MessageContent,TimeSent,MessageID,PhotoUrl FROM Messages where IsActive= '" + t + "' and MessageID= '" + id1 + "' ORDER BY TimeSent DESC", conn);
+        MySqlDataAdapter sda = new MySqlDataAdapter(cmd);
+        DataTable dt = new DataTable();
+        sda.Fill(dt);
+        foreach (DataRow dr in dt.Rows)
+        {
+            txtMail.Visible = true;
+            btnSendEmail.Visible = true;
+            txtReceiver.Visible = true;
+            lblmsg.Visible = true;
+            txtSubject.Visible = true;
+            lblsubject.Visible = true;
+            receiver = dr["SenderName"].ToString();
+            txtReceiver.Text = receiver;
+            
+       }
+        conn.Close();
+        cmd.Dispose();
 
-        txtMail.Visible = true;
-        btnSendEmail.Visible = true;
-        txtReceiver.Visible = true;
-        lblmsg.Visible = true;
-        txtSubject.Visible = true;
-        lblsubject.Visible = true;
-        receiver = m1.SenderName;
-        txtReceiver.Text = receiver;
     }
 
     protected void btnSendEmail_Click(object sender, EventArgs e)
     {
+       
+        string constr = ConfigurationManager.ConnectionStrings["Adroit"].ConnectionString;
+        conn = new MySqlConnection(constr);
+        conn.Open();
+        cmd = new MySqlCommand("SELECT *  FROM Teachers where Email= '" + Session["Email"].ToString() + "' ", conn);
+        rd = cmd.ExecuteReader();
+
+        if (rd.Read())
+        {
+            TID = rd.GetString("TeacherID");
+            TEmail = rd.GetString("Email");         
+            conn.Close();
+            cmd.Dispose();
+            rd.Close();
+        }
         string subject = txtSubject.Text;
         string body = txtMail.Text;
+   
+        MailMessage mail = new MailMessage();
+        mail.IsBodyHtml = true;
+        mail.BodyEncoding = System.Text.Encoding.UTF8;
         SmtpClient sc = new SmtpClient();
         sc.Port = 587;
+        //sc.Host = "student.cankaya.edu.tr";
         sc.Host = "smtp.gmail.com"; //smtp 
         sc.EnableSsl = true;
+        sc.UseDefaultCredentials = false;
+        //sc.Credentials = new NetworkCredential("c1311037@student.cankaya.edu.tr", "sifre");
 
-        sc.Credentials = new NetworkCredential("nzlks95@gmail.com", "sifre");
+        //mail.From = new MailAddress("c1311037@student.cankaya.edu.tr","Nazlı");
 
-        MailMessage mail = new MailMessage();
+        sc.Credentials = new NetworkCredential("naztim2111@gmail.com", "sifre");
 
-        mail.From = new MailAddress("nzlks95@gmail.com", "Nazlı");
-        
+        mail.From = new MailAddress("naztim2111@gmail.com", "Nazlı");
         mail.To.Add(receiver);
         mail.CC.Add("nzlks95@gmail.com");
 
@@ -181,12 +265,32 @@ public partial class Inbox : System.Web.UI.Page
         mail.CC.Add("c1311006@student.cankaya.edu.tr");
 
         mail.Subject = subject;
-        mail.IsBodyHtml = true;
+      
         mail.Body = body;
 
         if (body!="")
         {
+          
             sc.Send(mail);
+            mail.Dispose();
+
+            String connString = System.Configuration.ConfigurationManager.ConnectionStrings["Adroit"].ToString();
+            conn = new MySql.Data.MySqlClient.MySqlConnection(connString);
+            conn.Open();
+   
+            string CmdText = "INSERT INTO Messages(SenderName,TeacherID,MessageContent,TimeSent,ReceiverName,IsActive,PhotoUrl)VALUES(@SenderName,@TeacherID, @MessageContent, @TimeSent, @ReceiverName,1, @PhotoUrl)";
+            cmd = new MySqlCommand(CmdText, conn);
+
+            cmd.Parameters.AddWithValue("@SenderName", TEmail);
+            cmd.Parameters.AddWithValue("@TeacherID", TID);
+            cmd.Parameters.AddWithValue("@MessageContent", body);
+            cmd.Parameters.AddWithValue("@ReceiverName", receiver);
+            cmd.Parameters.AddWithValue("@PhotoUrl", "sss");
+            cmd.Parameters.AddWithValue("@TimeSent", DateTime.Now);
+
+            cmd.ExecuteNonQuery();
+            conn.Close();
+            cmd.Dispose();
             lblmessage.Text = "The Message Was Sent!";
             txtReceiver.Text = " ";
             txtSubject.Text = " ";
@@ -201,5 +305,5 @@ public partial class Inbox : System.Web.UI.Page
         }
         
     }
-    
+
 }
